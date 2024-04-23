fd9ee32173b3f8200f1987b698d5c5ba752f2ce7
Ikramul-m <i20727172@gmail.com>
2024-04-23 20:13:25 +0530

    CRUD Operation

--- Thapa/index.js
+++ Thapa/index.js
@@ -11,3 +11,5 @@ fs.appendFileSync("Bio.txt", "\nHe is very helpfull");
 
 const data = fs.readFileSync("Bio.txt","utf8"); // utf8 without Buffer
 console.log(data);
+
+fs.renameSync("Bio.txt","Biodata.txt") // Rename th txt file from Bio to Biodata
