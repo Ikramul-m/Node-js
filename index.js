df1eaf119a59abaca2165d5ed0846e604b66e9a2
Ikramul-m <i20727172@gmail.com>
2024-04-23 14:25:18 +0530

    Lesson

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const fs  = require("fs")
+const fs = require("fs");
 
-fs.writeFileSync("read.txt","Wlecome to my channel Ikram Technical")
-fs.writeFileSync("hello.py","print()")
+const buf_data = fs.readFileSync("Read.txt");
+
+// console.log(buf_data);
+
+org_data = buf_data.toString();
+console.log(org_data);
+
+// <Buffer 57 6c 65 63 6f 6d 65 20 74 6f 20 6d 79 20 63 68 61 6e 6e 65 6c 20 49 6b 72 61 6d 20 54 65 63 68 6e 69 63 61 6c>
