caffc3b6ba6e091726adcb5008d46bb098ff988e
Ikramul-m <i20727172@gmail.com>
2024-04-23 20:09:04 +0530

    CRUD operation

---
+++ Thapa/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+// fs.mkdirSync('Thapa')
+
+fs.writeFileSync(
+  "Bio.txt",
+  "Name: Thapa\nAge: 24\nProfession: Engineer\nDepartment: Computer Science & Engineering"
+);
+
+fs.appendFileSync("Bio.txt", "\nHe is very helpfull");
+
+const data = fs.readFileSync("Bio.txt","utf8");
+console.log(data);
