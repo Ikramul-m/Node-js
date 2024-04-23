2139acf38b142ca11996a4ed1fd23a9d0702a6f9
Ikramul-m <i20727172@gmail.com>
2024-04-23 14:28:45 +0530

    Lesson

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ org_data = buf_data.toString();
 console.log(org_data);
 
 // <Buffer 57 6c 65 63 6f 6d 65 20 74 6f 20 6d 79 20 63 68 61 6e 6e 65 6c 20 49 6b 72 61 6d 20 54 65 63 68 6e 69 63 61 6c>
+
+fs.writeFileSync("Read.txt", "ReadWrite.txt")
+fs.renameSync("Read.txt", "ReadWrite.txt")
