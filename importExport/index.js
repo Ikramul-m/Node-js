5732eaf621ec7809c8a50a254960601b10168a5e
Ikramul-m <i20727172@gmail.com>
2024-04-27 12:22:42 +0530

    Import Export

--- importExport/index.js
+++ importExport/index.js
@@ -1,4 +1,6 @@
-const {add,sub} = require("./open");
+const {add,sub,mul} = require("./open");
 
 console.log(add(5,5))
 console.log(sub(5,5))
+console.log(mul(5,5))
+
--- importExport/open.js
+++ importExport/open.js
@@ -6,7 +6,12 @@ const sub = (a,b) => {
     return a-b;
 }
 
+const mul = (a,b) => {
+    return a*b
+}
+
 module.exports.add = add
 module.exports.sub = sub
+module.exports.mul = mul
 
 
