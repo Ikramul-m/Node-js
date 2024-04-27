ca7455519adfdac5c5750507989cc9be68b651ad
Ikramul-m <i20727172@gmail.com>
2024-04-27 11:42:50 +0530

    PATH Module

--- Path/Path.js
+++ Path/Path.js
@@ -5,3 +5,9 @@ console.log(path.basename("C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs\ Path\ Pat
 console.log(path.extname("C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs\ Path\ Path.js"));
 
 console.log(path.parse("C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs\ Path\ Path.js"))
+
+const myPath = path.parse("C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs\ Path\ Path.js")
+console.log(myPath.name)
+console.log(myPath.dir)
+console.log(myPath.ext)
+console.log(myPath.base)
