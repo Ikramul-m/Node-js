ec633f14186005bc955ce64f0e63d48d30e9926d
Ikramul-m <i20727172@gmail.com>
2024-04-28 21:43:10 +0530

    Nodejs

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const c = path.dirname('C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs \ index.js')
 console.log(a)
 console.log(b)
 console.log(c)
+
+
+const pokemon = require("pokemon");
+
+// console.log(pokemon.all());
+console.log(pokemon.random());
