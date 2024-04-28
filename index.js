f09b3df873caaf7e608856349ef47a5005bf1146
Ikramul-m <i20727172@gmail.com>
2024-04-28 21:46:12 +0530

    Nodejs

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const path = require("path")
+// const path = require("path");
 
-const a = path.extname('C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs \ index.js')
-const b = path.basename('C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs \ index.js')
-const c = path.dirname('C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs \ index.js')
+// const a = path.extname("C: Users ikram Desktop Lessons Nodejs  index.js");
+// const b = path.basename("C: Users ikram Desktop Lessons Nodejs  index.js");
+// const c = path.dirname("C: Users ikram Desktop Lessons Nodejs  index.js");
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// const pokemon = require("pokemon");
+
+// // console.log(pokemon.all());
+// console.log(pokemon.random());
 
 
-const pokemon = require("pokemon");
-
-// console.log(pokemon.all());
-console.log(pokemon.random());
