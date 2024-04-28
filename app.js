1d085fd0302d44107d2597f145a66355f4cdd2d9
Ikramul-m <i20727172@gmail.com>
2024-04-28 21:26:21 +0530

    Nodejs

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const os = require("os");
 
 console.log("Free Memory: " + os.freemem());
-console.log("Host Name: " + os.hostname());
+console.log("Host Name:   " + os.hostname());
+console.log("Total Memoery: " + os.totalmem());
+
