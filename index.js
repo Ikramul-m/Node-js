33162e8146bf96a3820046e5b107541402e02428
Ikramul-m <i20727172@gmail.com>
2024-04-28 20:40:16 +0530

    Nodejs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const path = require("path")
 
 const a = path.extname('C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs \ index.js')
 const b = path.basename('C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs \ index.js')
+const c = path.dirname('C:\ Users\ ikram\ Desktop\ Lessons\ Nodejs \ index.js')
 
 console.log(a)
 console.log(b)
+console.log(c)
