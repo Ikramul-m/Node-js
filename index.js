3cbcf2f6ca2db0d2790d3864c9ff4b674c2f2637
Ikramul-m <i20727172@gmail.com>
2024-04-29 20:56:14 +0530

    Nodejs

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// const path = require("path");
+const http = require("http");
 
-// const a = path.extname("C: Users ikram Desktop Lessons Nodejs  index.js");
-// const b = path.basename("C: Users ikram Desktop Lessons Nodejs  index.js");
-// const c = path.dirname("C: Users ikram Desktop Lessons Nodejs  index.js");
+const PORT = 4000;
+const hostName = "localhost";
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+const server = http.createServer((request, response) => {
+  response.end("Working");
+});
 
-// const pokemon = require("pokemon");
-
-// // console.log(pokemon.all());
-// console.log(pokemon.random());
-
-const htpp = require('http')
-
-const server = htpp.createServer((request,response) => {
-    response.end("working...")
-})
-
-server.listen(4000,'localhost',() => {
-    console.log('Server is working on http://localhost:4000')
-})
+server.listen(PORT, hostName, () => {
+  console.log("Server is working on http://localhost:4000");
+});
