c174c9267b1db02227eea20736794bf8866b4018
Ikramul-m <i20727172@gmail.com>
2024-04-30 20:34:52 +0530

    Nodejs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = 4000;
 const hostName = "localhost";
 
+const home = fs.readFileSync("./index.html", "utf-8");
+
 const server = http.createServer((request, response) => {
   if (request.url === "/") {
-    response.end("<h1>Home Page</h1>");
+    response.end(home);
   } else if (request.url === "/contact") {
     response.end("<h1>Contact</h1>");
   } else if (request.url === "/about") {
